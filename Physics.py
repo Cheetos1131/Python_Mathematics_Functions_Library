bbb3b073000494e2811c8c52d64dbdc39ab248f1
Cheetos1131 <68630002+Cheetos1131@users.noreply.github.com>
2021-06-15 13:13:43 -0400

    Add files via upload
    Added new equations and added comments for improved readability

--- Physics.py
+++ Physics.py
@@ -1,98 +1,129 @@
 import math
 
-def Linear_noD():
-    selector = int(input("Enter 1 to solve for Vf, 2 for Vo, 3 for a, or 4 for t. "))
-    if (selector == 1): 
+def Linear_noD(): # This is the Linear Equation without Distance as a variable
+    selector = int(input("Enter 1 to solve for Vf, 2 for Vo, 3 for a, or 4 for t. ")) # This asks the user for what variable they are missing and selects an equation based on this input
+    if (selector == 1): # Equation to solve final velocity
         Vo = float(input("Enter initial velocity in terms of m/s. "))
         a = float(input("Enter acceleration in terms of m/s^2. "))
         t = int(input("Enter total time in terms of seconds. "))
         Vf = (Vo + a) * t
         print("The final velocity is " + str(Vf) + " m/s.")
-    elif (selector == 2):
+    elif (selector == 2): # Equation to solve initial velocity
         Vf = float(input("Enter the final velocity in terms of m/s. "))
         a = float(input("Enter the acceleration in terms of m/s^2. "))
         t = int(input("Enter the time in terms of seconds. "))
         Vo = -Vf + a * t 
         print(Vo)
-    elif (selector == 3): 
+    elif (selector == 3): # Equation to solve acceleration
         Vf = float(input("Enter the final velocity in terms of m/s. "))
         Vo = float(input("Enter the initial velocity in terms of m/s. "))
         t = int(input("Enter time in terms of seconds. "))
         a = (Vf - Vo) / t
         print(a)
-    elif (selector == 4):
+    elif (selector == 4): # Equation to solve time
         Vf = float(input("Enter the final velocity in terms of m/s. "))
         Vo = float(input("Enter the initial velocity in terms of m/s. "))
         a = float(input("Enter the acceleration in terms of m/s^2. "))
         t = (Vf - Vo) / a
         print(t)
-    else: 
+    else: # In case the user input something that was not in the program
         print("Retry and enter one of the variables.")
 
-def Linear_noT():
-    selector = int(input("Enter 1 to solve for Vf, 2 for Vo, 3 for a, or 4 for d. ")) 
-    if (selector == 1):
+def Linear_noT(): # This is the Linear Equation without Time as a variable
+    selector = int(input("Enter 1 to solve for Vf, 2 for Vo, 3 for a, or 4 for d. ")) # This asks the user for what variable they are missing and selects an equation based on this input
+    if (selector == 1): # Equation to solve final velocity
         Vo = float(input("Enter in the initial velocity in terms of m/s. "))
         a = float(input("Enter in the acceleration in terms of m/s^2. "))
         d = float(input("Enter in the distance in terms of m. "))
         Vf = Vo + math.sqrt(2 * a * d)
         print(Vf)
-    elif (selector == 2):
+    elif (selector == 2): # Equation to solve initial velocity
         Vf = float(input("Enter in the final velocity in terms of m/s. "))
         a = float(input("Enter in the acceleration in terms of m/s^2. "))
         d = float(input("Enter in the distance in terms of m. "))
         Vo = Vf - math.sqrt(2 * a * d) 
         print(Vo)
-    elif (selector == 3):
+    elif (selector == 3): # Equation to solve acceleration
         Vf = float(input("Enter in the final velocity in terms of m/s. "))
         Vo = float(input("Enter in the initial velocity in terms of m/s. "))
         d = float(input("Enter in the distance in terms of m. "))
         a = math.sqrt((pow(Vf, 2) - pow(Vo, 2)) / (2 * d)) 
         print(a)
-    elif (selector == 4):
+    elif (selector == 4): # Equation to solve distance
         Vf = float(input("Enter in the final velocity in terms of m/s. "))
         Vo = float(input("Enter in the initial velocity in terms of m/s. "))
         a = float(input("Enter in the acceleration in terms of m/s^2. "))
         d = math.sqrt((pow(Vf, 2) - pow(Vo, 2)) / (2 * a)) 
         print(d)
-    else: 
+    else: # In case the user input something that was not in the program
         print("Retry and enter one of the variables.")
 
-def Linear_NoVf(): 
-    selector = int(input("Enter 1 to solve for Vo, 2 for t, 3 for a, or 4 for d. "))
-    if (selector == 1):
+def Linear_noVf(): # This is the Linear Equation without Final Velocity as a variable
+    selector = int(input("Enter 1 to solve for Vo, 2 for t, 3 for a, or 4 for d. ")) # This asks the user for what variable they are missing and selects an equation based on this input
+    if (selector == 1): # Equation to solve initial velocity
         d = float(input("Enter in the distance in terms of m. "))
         a = float(input("Enter in the acceleration in terms of m/s^2. "))
         t = int(input("Enter time in terms of seconds. "))
         Vo = (d - ((1 / 2) * a * pow(t, 2))) / t
         print(Vo)
-    elif (selector == 2): 
+    elif (selector == 2): # Equation to solve time
         d = float(input("Enter in the distance in terms of m. "))
         Vo = float(input("Enter in the initial velocity in terms of m/s. "))
         a = float(input("Enter in the acceleration in terms of m/s^2. "))
         t = (2*(d - Vo)) / a
         print(t)
-    elif (selector == 3):
+    elif (selector == 3): # Equation to solve acceleration
         d = float(input("Enter in the distance in terms of m. "))
         Vo = float(input("Enter in initial velocity in terms of m/s. "))
         t = int(input("Enter time in terms of seconds. "))
         a = (d - (Vo * t)) / ((1 / 2) * pow(t, 2))
         print(a)
-    elif (selector == 4): 
+    elif (selector == 4): # Equation to solve distance
         Vo = float(input("Enter in the initial velocity in terms of m/s. "))
         a = float(input("Enter in the acceleration in terms of m/s^2. "))
         t = int(input("Enter in the time in terms of seconds. "))
         d = (Vo * t) + ((1 / 2) * a * pow(t, 2))
         print(d)
-    else: 
+    else: # In case the user input something that was not in the program
+        print("Retry and enter one of the variables. ")
+        
+def Linear_noA(): # This is the Linear Equation without Acceleration as a variable
+    selector = int(input("Enter 1 to solve for d, 2 for Vo, 3 for Vf, or 4 for t. ")) # This asks the user for what variable they are missing and selects an equation based on this input
+    if (selector == 1): # Equation to solve distance
+        Vo = float(input("Enter in the initial velocity in terms of m/s. "))
+        Vf = float(input("Enter in the final velocity in terms of m/s. "))
+        t = float(input("Enter in the time in terms of seconds. "))
+        d = ((1 / 2) * (Vo + Vf)) * t
+        print(d)
+    elif (selector == 2): # Equation to solve initial velocity
+        d = float(input("Enter in the distance in terms of m. "))
+        t = float(input("Enter in the time in terms of seconds. "))
+        Vf = float(input("Enter in the final velocity in terms of m/s. "))
+        Vo = (2 * (d / t)) - Vf
+        print(Vo)
+    elif (selector == 3): # Equation to solve final velocity
+        d = float(input("Enter in the distance in terms of m. "))
+        t = float(input("Enter in the time in terms of seconds. "))
+        Vo = float(input("Enter in the initial velocity in terms of m/s. "))
+        Vf = (2 * (d / t)) - Vo
+        print(Vf)
+    elif (selector == 4): # Equation to solve time
+        d = float(input("Enter in the distance in terms of m. "))
+        Vo = float(input("Enter in the initial velocity in terms of m/s. "))
+        Vf = float(input("Enter in the final velocity in terms of m/s. "))
+        t = ((2 * d) / (Vo + Vf))
+        print(t) 
+    else: # In case the user input something that was not in the program
         print("Retry and enter one of the variables. ")
 
-eqSelect = int(input("Enter in a 1 for and equation without distance, 2 for without time, 3 for no final velocity. "))
+eqSelect = int(input("Enter in a 1 for and equation without distance, 2 for without time, 3 for no final velocity. ")) # Equation selector based on user input
 if (eqSelect == 1): 
     Linear_noD()
 elif (eqSelect == 2): 
     Linear_noT()
 elif (eqSelect == 3): 
-    Linear_NoVf()
-else: 
-    print("Retry and enter a number from 1 to 3. ") 
+    Linear_noVf()
+elif (eqSelect == 4): 
+    Linear_noA()
+else: # In case the user input something that was not in the program
+    print("Retry and enter a number from 1 to 4. ") 
